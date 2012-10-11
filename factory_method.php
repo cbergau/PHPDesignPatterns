7eb90b4dc78fe1621f0632dc388fb832714051d9
cbergau <cbergau86@gmail.com>
2012-10-11 15:44:43 +0200

    A concrete example of factory method pattern implementation.
    Change-Id: I0a795d22e49ecae92f11eadfe076efa14a99c8ae

--- factory_method.php
+++ factory_method.php
@@ -1,4 +1,53 @@
 <?php
 /**
- * Factory method pattern
+ * Factory method pattern example
+ *
+ * @author    Christian Bergau <cbergau86@gmail.com>
+ * @copyright Free for all
+ * @package   PHPDesignPatterns
  */
+
+interface ProductInterface
+{
+    public function getName();
+}
+
+class ConcreteProduct implements ProductInterface
+{
+    public function getName()
+    {
+        return 'ConcreteProduct';
+    }
+}
+
+class AnotherConcreteProduct implements ProductInterface
+{
+    public function getName()
+    {
+        return 'AnotherConcreteProduct';
+    }
+}
+
+class ProductFactory
+{
+    public function factory($type)
+    {
+        switch ($type)
+        {
+            case 'Concrete':
+                $product = new ConcreteProduct();
+                break;
+            case 'AnotherConcrete':
+                $product = new AnotherConcreteProduct();
+                break;
+            default:
+                throw new Exception('Invalid type');
+        }
+
+        return $product;
+    }
+}
+
+$productFactory = new ProductFactory();
+$product = $productFactory->factory('Concrete');
+echo $product->getName();
