feadbe94d6e00f97ab13e7e67d3c5f55abc0cdc9
cbergau <cbergau86@gmail.com>
2012-10-12 08:22:24 +0200

    Added command pattern implementation
    Change-Id: I6629ed2b11b14f7e4a96a5cb66085ec804d07115

---
+++ command.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Command pattern example
+*
+* @author    Christian Bergau <cbergau86@gmail.com>
+* @copyright Free for all
+* @package   PHPDesignPatterns
+* @link      http://en.wikipedia.org/wiki/Command_pattern
+*/
+
+class Invoker
+{
+    protected $commands = array();
+
+    public function addCommand($name, $commands)
+    {
+        $this->commands[$name] = $commands;
+    }
+
+    public function executeCommands($name, Receiver $receiver)
+    {
+        if (!isset($this->commands[$name])) {
+            throw new Exception('Invalid name');
+        }
+        foreach ($this->commands[$name] as $command) {
+            $command->execute($receiver);
+        }
+    }
+}
+
+interface CommandInterface
+{
+    public function execute(Receiver $receiver);
+}
+
+class FirstConcreteCommand implements CommandInterface
+{
+    public function execute(Receiver $receiver)
+    {
+        echo "First command on ".$receiver->getName();
+    }
+}
+
+class SecondConcreteCommand implements CommandInterface
+{
+    public function execute(Receiver $receiver)
+    {
+        echo "Second command on ".$receiver->getName();
+    }
+}
+
+class Receiver
+{
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
+$invoker = new Invoker();
+$invoker->addCommand(
+        'standard',
+        array(
+                new FirstConcreteCommand(),
+        )
+);
+
+$invoker->addCommand(
+        'deluxe',
+        array(
+                new FirstConcreteCommand(),
+                new SecondConcreteCommand()
+        )
+);
+
+$someReceiver = new Receiver('ReceiverOne');
+$invoker->executeCommands('standard', $someReceiver);
+
+$anotherReceiver = new Receiver('ReceiverTwo');
+$invoker->executeCommands('deluxe', $anotherReceiver);
