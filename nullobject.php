0035d4af52efa0f361818efc203f92cec1e26933
ingo <ingo.walz@googlemail.com>
2012-11-14 22:42:07 +0100

    Implementation of null object pattern
    Change-Id: Iaa1b16580bf802a4eedf098df87fd69a24966569

---
+++ nullobject.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Null Object Pattern
+ * http://en.wikipedia.org/wiki/Null_Object_pattern
+ */
+namespace Model {
+    interface BrainInterface {
+        public function setIQ($iq);
+        public function getIQ();
+    }
+    
+    class Brain implements BrainInterface {
+        protected $iq = null;
+
+        public function getIQ() {
+            return $this->iq;
+        }
+        
+        public function setIQ($iq) {
+            $this->iq = $iq;
+        }
+    }
+    
+    class NullBrain implements BrainInterface {
+        public function setIQ($iq) { }
+        public function getIQ() {
+            return "Sooo low? Really?";
+        }
+    }
+    
+    class BrainAdapter {
+        public function getByNumber($iq) {
+            if($iq <= 0)
+                return new NullBrain();
+            
+            $brain = new Brain();
+            $brain->setIQ($iq);
+            
+            return $brain;
+        }
+    }
+    
+    foreach(array(100, 40, 12, 0 , 30, -4) as $iq) {
+        $adapter = new BrainAdapter();
+        $brain = $adapter->getByNumber($iq);
+        
+        echo "Your IQ is: " . $brain->getIQ() . PHP_EOL;
+    }
+    
+}
+
