25377c82da179fd7d33dc46f7451fda2fcf3dac3
Chrisitan Bergau <cbergau86@gmail.com>
2015-08-10 16:18:53 +0200

    Add new NullObject Pattern example

--- nullobject.php
+++ nullobject.php
@@ -1,66 +1,80 @@
 <?php
 /**
- * Null Object Pattern
+ * NullObject pattern example
  *
- * @author    Ingo Walz
+ * @author    Christian Bergau <c.bergau@netrada.com>
  * @copyright Free for all
- * @link      http://en.wikipedia.org/wiki/Null_Object_pattern
+ * @link      https://en.wikipedia.org/wiki/Null_Object_pattern
  */
 
-namespace Model
+interface Employee
 {
-    interface BrainInterface
-    {
-        public function setIQ($iq);
-        public function getIQ();
-    }
-    
-    class Brain implements BrainInterface
-    {
-        protected $iq;
+    /**
+     * @return int
+     */
+    public function getId();
 
-        public function getIQ()
-        {
-            return $this->iq;
-        }
-        
-        public function setIQ($iq)
-        {
-            $this->iq = $iq;
-        }
-    }
-    
-    class NullBrain implements BrainInterface
-    {
-        public function setIQ($iq)
-        {
-        }
+    /**
+     * @return string
+     */
+    public function getName();
 
-        public function getIQ()
-        {
-            return "Sooo low? Really?";
-        }
-    }
-    
-    class BrainAdapter
+    /**
+     * @return double
+     */
+    public function getSalary();
+}
+
+class EmployeeImpl implements Employee
+{
+    private $id;
+    private $name;
+    private $salary;
+
+    /**
+     * @param int    $id
+     * @param string $name
+     * @param double $salary
+     */
+    public function __construct($id, $name, $salary)
     {
-        public function getByNumber($iq)
-        {
-            if ($iq <= 0) {
-                return new NullBrain();
-            }
-            
-            $brain = new Brain();
-            $brain->setIQ($iq);
-            
-            return $brain;
-        }
+        $this->id = $id;
+        $this->name = $name;
+        $this->salary = $salary;
     }
-    
-    foreach(array(100, 40, 12, 0 , 30, -4) as $iq) {
-        $adapter = new BrainAdapter();
-        $brain = $adapter->getByNumber($iq);
-        
-        echo "Your IQ is: " . $brain->getIQ() . PHP_EOL;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getSalary()
+    {
+        return $this->salary;
+    }
+}
+
+// THIS IS THE NULL OBJECT:
+
+class NullEmployee implements Employee
+{
+    public function getId()
+    {
+        return 0;
+    }
+
+    public function getName()
+    {
+        return '';
+    }
+
+    public function getSalary()
+    {
+        return 0.0;
     }
 }
