9a811bbb15ebb0479925cabb3680d50927dd4173
Chrisitan Bergau <cbergau86@gmail.com>
2015-08-10 16:26:09 +0200

    Implement Singleton

--- singleton.php
+++ singleton.php
@@ -1,4 +1,32 @@
 <?php
 
-// NEVER USE SINGLETONS!
-// http://blogs.msdn.com/b/scottdensmore/archive/2004/05/25/140827.aspx
+class Singleton
+{
+    /**
+     * @var Singleton
+     */
+    protected static $instance;
+
+    protected function __construct()
+    {
+    }
+
+    protected function __clone()
+    {
+    }
+
+    /**
+     * @return Singleton
+     */
+    public static function getInstance()
+    {
+        if (!static::$instance) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+}
+
+$s = Singleton::getInstance();
+var_dump($s);
